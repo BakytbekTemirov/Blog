99d9f174287a1c9b1730e50b9b6d2854aa380106
BakytbekTemirov <b.temirow@yahoo.com>
2022-09-02 09:48:32 +0600

    Save now

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import smtplib
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "ksdaskjhdjkshd23213123")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
